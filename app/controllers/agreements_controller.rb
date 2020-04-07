84febf60eaadbf55692d709ecb6fa6d9ad496a0e
yuki-kasai <yuki000kasai000@gmail.com>
2020-04-08 06:35:46 +0900

    add: create

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -7,8 +7,21 @@ class AgreementsController < ApplicationController
   end
 
   def new
+    @agreement = Agreement.new
+  end
+
+  def create
+    agreement = Agreement.new(agreement_params)
+    agreement.save!
+    redirect_to agreements_url, notice: "「#{agreement.servise_name}」を登録しました。"
   end
 
   def edit
+  end
+
+  private
+
+  def agreement_params
+    params.require(:agreement).permit(:servise_name, :price)
   end
 end
