565d9e5c0faea83a9a7cbde771d4669a2c25bdbc
yuki-kasai <yuki000kasai000@gmail.com>
2020-04-10 21:36:25 +0900

    fix: create_delete

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -26,6 +26,12 @@ class AgreementsController < ApplicationController
     redirect_to agreements_url, notice: "タスク「#{agreement.servise_name}」を更新しました。"
   end
 
+  def destroy
+    agreement = Agreement.find(params[:id])
+    agreement.destroy
+    redirect_to agreement_url, notice: "サービス「#{agreement.servise_name}」を削除しました。"
+  end
+
   private
 
   def agreement_params
