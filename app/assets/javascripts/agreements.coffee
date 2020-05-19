aadc3d1781ca8aa7b14469a528307e12272d4343
yuki-kasai <yuki000kasai000@gmail.com>
2020-05-20 06:35:11 +0900

    fix: chart

--- app/assets/javascripts/agreements.coffee
+++ app/assets/javascripts/agreements.coffee
@@ -4,37 +4,23 @@
 
 window.draw_graph = ->
     ctx = document.getElementById("myChart").getContext('2d')
-    barNum = 13
-    labels = new Array(barNum)
-    bgColors = new Array(barNum)
-    bdColors = new Array(barNum)
-    for i in [1...barNum]
-        labels[i] =  i + '月'
-        bgColors[i] = 'rgba(75, 192, 192, 0.2)'
-        bdColors[i] = 'rgba(75, 192, 192, 1)'
+    month = 12
+    labels = new Array(month)
+    bgColors = new Array(month)
+    bdColors = new Array(month)
+    for i in [0...month]
+        labels[i] =  i + 1 + '月'
+        bgColors[i] = 'rgba(255, 99, 132, 0.2)'
+        bdColors[i] = 'rgba(153, 102, 255, 1)'
     myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
             datasets: [{
                 label: '月額利用料',
-                data: gon.linedata,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                data: gon.data,
+                backgroundColor: bgColors,
+                borderColor: bdColors,
                 borderWidth: 1
             }]
         },
--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -5,56 +5,100 @@ class AgreementsController < ApplicationController
     @q = Agreement.ransack(params[:q])
     @agreements = @q.result(distinct: true).order(service_name: :asc).page(params[:page]).per(5)
 
-    # gon.data2 = []
-    # gon.data3 = []
-    # gon.data4 = []
-    # gon.data5 = []
-    # gon.data6 = []
-    # gon.data7 = []
     # 12.times do
 
     # from  = Time.now.at_beginning_of_day
     # to    = (from - 1.month)
     # gon.data = Agreement.where(created_at: from...to).sum(:price)
-      # gon.data << Agreement.where(created_at: search_date.in_time_zone.all_month).sum(:price)
+    # gon.data << Agreement.where(created_at: search_date.in_time_zone.all_month).sum(:price)
 
       # time = Time.now - 1.month
       # this_month = time.beginning_of_month..time.end_of_month
       # gon.data << Agreement.where(created_at: this_month).sum(:price)
 
       # gon.data = []
-      gon.data2 = []
-      gon.data3 = []
-      gon.data4 = []
-      gon.data5 = []
-      gon.data6 = []
-      gon.data7 = []
+      # labels = { 0: "1月", 1: "2月", 2: "3月", 3: "4月", 4: "5月", 5: "6月", 6: "7月", 7: "8月", 8: "9月", 9: "10月",10: "11月", 11: "12月" }
 
+      # first_month = '2020-04-01'
 
-      gon.data = []
+      # t = Time.new
+      #
+      # gon.data = []
+      #
+      # sum = 0
+      # gon.bardata = []
+      # gon.linedata = []
+      #
+      # 12.times do |i|
+      #   # 月の合計金額
+      #
+      #   data = rand(100.0)
+      #   gon.data << data
+      #   # 毎月の合計金額
+      #   gon.bardata << data
+      #   sum = sum + data
+      #   gon.linedata << sum
+      # end
 
+      sum = 0
+      time = Time.now - 1.month
+      this_month = time.beginning_of_month..time.end_of_month
       first_month = ''
+      # array = []
+      # array = Agreement.where(created_at: this_month).split("")
 
-      t = Time.new
+#       Agreement.all.each do |i|
+#         gon.data = []
+#         # 取得月のデータの合計金額
+#         # target_month = a.created_at.strftime("%m")
+#         # target_month = target_month.to_i
+#         # gon.data[target_month - 1] = gon.data[target_month - 1] + Agreement.where(created_at: this_month).sum(:price)
+#           target_month = i.price
+#           sum = sum + target_month
+#           gon.data.unshift(sum)
+# binding.pry
+#       end
 
-      Agreement.all.each do |a|
-        target_month = t.strftime("%m")
-          binding.pry
-        gon.data[target_month - 1] = gon.data[target_month - 1] + a.price
+sum = 0
+time = Time.now - 1.month
+this_month = time.beginning_of_month..time.end_of_month
+gon.data = []
+first_month = 0
 
-      end
+Agreement.where(created_at: this_month).each do |a|
+  target_month = a.created_at.strftime("%m")
+  target_month = target_month.to_i
+  sum = sum + a.price
+  gon.data[target_month - 1] = sum
+  gon.data[target_month] = Agreement.sum(:price)
 
-      # time = Time.now
-      # this_month = time.beginning_of_month..time.end_of_month
-      # gon.data << Agreement.where(created_at: this_month).sum(:price)
-      # gon.data2 << Agreement.all.sum(:price)
-      # gon.data3 << Agreement.all.sum(:price)
-      # gon.data4 << Agreement.all.sum(:price)
-      # gon.data5 << Agreement.all.sum(:price)
-      # gon.data6 << Agreement.all.sum(:price)
-      # gon.data7 << Agreement.all.sum(:price)
-    # end
+  first_month = [first_month, target_month].min
+end
 
+# binding.pry
+      # array = [50000, 10000, 20000]
+
+# binding.pry
+      # aのインスタンスのcreated_atの月のみを取得する
+      # target_month = a.created_at.strftime("%m")
+      # target_month = target_month.to_i
+      # binding.pry
+      # gon.data[target_month - 1] = gon.data[target_month - 1] + a.price
+      # # 最小月を随時更新
+      # first_month = [first_month, target_month].min
+
+      # Agreement.all.each do |a|
+      #   target_month = a.created_at
+      #   target_month = target_month.beginning_of_month..target_month.end_of_month
+      #   binding.pry
+      #
+      #   target_month = target_month.to_i
+      #   Agreement.where(created_at: a.created_at.strftime("%m")).sum(:price)
+      #   gon.data << a.price
+      #
+      #   # gon.data[target_month - 1] = gon.data[target_month - 1] << a.price
+      # end
+        # binding.pry
   end
 
   def show
