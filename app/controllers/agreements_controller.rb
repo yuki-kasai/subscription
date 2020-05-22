839947ddbbc1047fb02d059f4a51a358e167c872
yuki-kasai <yuki000kasai000@gmail.com>
2020-05-22 16:13:25 +0900

    fix: i18n

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -5,100 +5,21 @@ class AgreementsController < ApplicationController
     @q = Agreement.ransack(params[:q])
     @agreements = @q.result(distinct: true).order(service_name: :asc).page(params[:page]).per(5)
 
-    # 12.times do
+    sum = 0
+    time = Time.now - 1.month
+    this_month = time.beginning_of_month..time.end_of_month
+    gon.data = []
+    first_month = 0
 
-    # from  = Time.now.at_beginning_of_day
-    # to    = (from - 1.month)
-    # gon.data = Agreement.where(created_at: from...to).sum(:price)
-    # gon.data << Agreement.where(created_at: search_date.in_time_zone.all_month).sum(:price)
+    Agreement.where(created_at: this_month).each do |a|
+      target_month = a.created_at.strftime("%m")
+      target_month = target_month.to_i
+      sum = sum + a.price
+      gon.data[target_month - 1] = sum
+      gon.data[target_month] = Agreement.sum(:price)
 
-      # time = Time.now - 1.month
-      # this_month = time.beginning_of_month..time.end_of_month
-      # gon.data << Agreement.where(created_at: this_month).sum(:price)
-
-      # gon.data = []
-      # labels = { 0: "1月", 1: "2月", 2: "3月", 3: "4月", 4: "5月", 5: "6月", 6: "7月", 7: "8月", 8: "9月", 9: "10月",10: "11月", 11: "12月" }
-
-      # first_month = '2020-04-01'
-
-      # t = Time.new
-      #
-      # gon.data = []
-      #
-      # sum = 0
-      # gon.bardata = []
-      # gon.linedata = []
-      #
-      # 12.times do |i|
-      #   # 月の合計金額
-      #
-      #   data = rand(100.0)
-      #   gon.data << data
-      #   # 毎月の合計金額
-      #   gon.bardata << data
-      #   sum = sum + data
-      #   gon.linedata << sum
-      # end
-
-      sum = 0
-      time = Time.now - 1.month
-      this_month = time.beginning_of_month..time.end_of_month
-      first_month = ''
-      # array = []
-      # array = Agreement.where(created_at: this_month).split("")
-
-#       Agreement.all.each do |i|
-#         gon.data = []
-#         # 取得月のデータの合計金額
-#         # target_month = a.created_at.strftime("%m")
-#         # target_month = target_month.to_i
-#         # gon.data[target_month - 1] = gon.data[target_month - 1] + Agreement.where(created_at: this_month).sum(:price)
-#           target_month = i.price
-#           sum = sum + target_month
-#           gon.data.unshift(sum)
-# binding.pry
-#       end
-
-sum = 0
-time = Time.now - 1.month
-this_month = time.beginning_of_month..time.end_of_month
-gon.data = []
-first_month = 0
-
-Agreement.where(created_at: this_month).each do |a|
-  target_month = a.created_at.strftime("%m")
-  target_month = target_month.to_i
-  sum = sum + a.price
-  gon.data[target_month - 1] = sum
-  gon.data[target_month] = Agreement.sum(:price)
-
-  first_month = [first_month, target_month].min
-end
-
-# binding.pry
-      # array = [50000, 10000, 20000]
-
-# binding.pry
-      # aのインスタンスのcreated_atの月のみを取得する
-      # target_month = a.created_at.strftime("%m")
-      # target_month = target_month.to_i
-      # binding.pry
-      # gon.data[target_month - 1] = gon.data[target_month - 1] + a.price
-      # # 最小月を随時更新
-      # first_month = [first_month, target_month].min
-
-      # Agreement.all.each do |a|
-      #   target_month = a.created_at
-      #   target_month = target_month.beginning_of_month..target_month.end_of_month
-      #   binding.pry
-      #
-      #   target_month = target_month.to_i
-      #   Agreement.where(created_at: a.created_at.strftime("%m")).sum(:price)
-      #   gon.data << a.price
-      #
-      #   # gon.data[target_month - 1] = gon.data[target_month - 1] << a.price
-      # end
-        # binding.pry
+      first_month = [first_month, target_month].min
+    end
   end
 
   def show
