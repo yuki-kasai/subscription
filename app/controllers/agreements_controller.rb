a301e3cc9dc90e3c32d4bcbfd562981bfaf4216a
yuki-kasai <yuki000kasai000@gmail.com>
2020-06-01 06:25:53 +0900

    fix: current_user

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -1,26 +1,25 @@
 class AgreementsController < ApplicationController
-  # before_action :set_agreement, only: [:show, :edit, :update, :destroy]
+  before_action :set_current_user_agreement, only: [:show, :edit, :update, :destroy]
 
   def index
-    @q = Agreement.ransack(params[:q])
+    @q = Agreement.where(user_id: current_user.id).ransack(params[:q])
     @agreements = @q.result(distinct: true).order(service_name: :asc).page(params[:page]).per(5)
+    @agreements_chart = @q.result(distinct: true)
 
     sum = 0
     time = Time.now - 1.month
     this_month = time.beginning_of_month..time.end_of_month
     gon.data = []
-    first_month = 0
 
-    Agreement.all.each do |a|
-      target_month = a.created_at.strftime("%m")
-      target_month = target_month.to_i
+    @agreements_chart.all.each do |a|
+      target_month = a.created_at.strftime("%m").to_i
+
       gon.data[target_month - 1] = gon.data[target_month - 1].to_i + a.price
-      first_month = [first_month, target_month].min
     end
   end
 
   def show
-    # @agreement = Agreement.find(params[:id])
+    @agreement = Agreement.find(params[:id])
   end
 
   def new
@@ -28,9 +27,10 @@ class AgreementsController < ApplicationController
   end
 
   def create
-    agreement = Agreement.new(agreement_params)
-    agreement.save!
-    redirect_to agreements_url, notice: "「#{agreement.service_name}」を登録しました。"
+    @agreement = Agreement.new(agreement_params)
+    @agreement.user_id = current_user.id
+    @agreement.save
+    redirect_to agreements_url, notice: "「#{@agreement.service_name}」を登録しました。"
   end
 
   def edit
@@ -52,10 +52,10 @@ class AgreementsController < ApplicationController
   private
 
   def agreement_params
-    params.require(:agreement).permit(:service_name, :price)
+    params.require(:agreement).permit(:service_name, :price, :user_id)
   end
 
-  # def set_agreement
-  #   @agreement = Agreement.find(params[:id])
-  # end
+  def set_current_user_agreement
+    @agreement = current_user.agreements.find(params[:id])
+  end
 end
